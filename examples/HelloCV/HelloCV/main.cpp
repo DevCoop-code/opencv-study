008c975533e83228a0dddba6df5432e138328407
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-12-20 19:09:59 +0900

    feat: Success to make Showing image

--- examples/HelloCV/HelloCV/main.cpp
+++ examples/HelloCV/HelloCV/main.cpp
@@ -6,10 +6,24 @@
 //
 
 #include <iostream>
-#include <opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, OpenCV " << CV_VERSION << std::endl;
+    
+    cv::Mat img;
+    img = cv::imread("/Users/hangyojeong/Desktop/maxresdefault.jpg");
+    
+    if(img.empty()) {
+        std::cerr << "Image load failed!" << std::endl;
+        return -1;
+    }
+    
+    cv::namedWindow("Image");
+    cv::imshow("Image", img);
+    
+    cv::waitKey();
+    
     return 0;
 }
