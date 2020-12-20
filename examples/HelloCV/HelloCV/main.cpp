02452bc7e106f67b7cc4df0630a4d82ed560ee92
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-12-20 18:12:16 +0900

    feat: Example of Simple OpenCV

---
+++ examples/HelloCV/HelloCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloCV
+//
+//  Created by HanGyo Jeong on 2020/12/20.
+//
+
+#include <iostream>
+#include <opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, OpenCV " << CV_VERSION << std::endl;
+    return 0;
+}
