621c1449b1b8a7c38b99b46bb241b9f0970ada3a
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-06-15 10:23:42 +0900

    feat: Templete Matching

--- examples/HelloCV/HelloCV/main.cpp
+++ examples/HelloCV/HelloCV/main.cpp
@@ -19,6 +19,7 @@ void contours_basic();
 void contours_hier();
 void setLabel(Mat& img, const vector<Point>& pts, const String& label);
 void PolygonDetecting();
+void template_matching();
 
 int main(int argc, const char * argv[]) {
     /*
@@ -88,7 +89,12 @@ int main(int argc, const char * argv[]) {
     /*
     Detecting out-line
      */
-    PolygonDetecting();
+//    PolygonDetecting();
+    
+    /*
+     Templete Matching
+     */
+    template_matching();
     
     waitKey();
     
@@ -300,6 +306,43 @@ void PolygonDetecting()
         }
     }
     
+    imshow("img", img);
+    
+    waitKey();
+    destroyAllWindows();
+}
+
+void template_matching()
+{
+    Mat img = imread("/Users/hangyojeong/Desktop/circuit.bmp", IMREAD_COLOR);
+    Mat templ = imread("/Users/hangyojeong/Desktop/crystal.bmp", IMREAD_COLOR);
+    
+    if (img.empty() || templ.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+    
+    img = img + Scalar(50, 50, 50);
+    
+    // Add Gaussian Noise which has Standard Deviation value 10
+    Mat noise(img.size(), CV_32SC3);
+    randn(noise, 0, 10);
+    add(img, noise, img, Mat(), CV_8UC3);
+    
+    Mat res, res_norm;
+    matchTemplate(img, templ, res, TM_CCOEFF_NORMED);
+    normalize(res, res_norm, 0, 255, NORM_MINMAX, CV_8U);
+    
+    double maxv;
+    Point maxloc;
+    minMaxLoc(res, 0, &maxv, 0, &maxloc);
+    cout << "maxv: " << maxv << endl;
+    
+    rectangle(img, Rect(maxloc.x, maxloc.y, templ.cols, templ.rows), Scalar(0, 0, 255), 2);
+    
+    imshow("templ", templ);
+    imshow("res_norm", res_norm);
     imshow("img", img);
     
     waitKey();
