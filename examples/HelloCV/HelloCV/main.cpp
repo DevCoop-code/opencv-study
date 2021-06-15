0c4b881c8463cccc97031e2a241af35dbb36f395
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-06-15 09:39:10 +0900

    feat: Detecting polygon

--- examples/HelloCV/HelloCV/main.cpp
+++ examples/HelloCV/HelloCV/main.cpp
@@ -15,6 +15,10 @@ void on_threshold(int pos, void* userdata);
 void erode_dilate();
 void open_close();
 void labeling_stats();
+void contours_basic();
+void contours_hier();
+void setLabel(Mat& img, const vector<Point>& pts, const String& label);
+void PolygonDetecting();
 
 int main(int argc, const char * argv[]) {
     /*
@@ -73,7 +77,18 @@ int main(int argc, const char * argv[]) {
     /*
     Labeing
      */
-    labeling_stats();
+//    labeling_stats();
+    
+    /*
+     Contour
+     */
+//    contours_basic();
+//    contours_hier();
+    
+    /*
+    Detecting out-line
+     */
+    PolygonDetecting();
     
     waitKey();
     
@@ -166,6 +181,126 @@ void labeling_stats()
     
     imshow("src", src);
     imshow("dst", dst);
+    
+    waitKey();
+    destroyAllWindows();
+}
+
+void contours_basic()
+{
+    Mat src = imread("/Users/hangyojeong/Desktop/contours.bmp", IMREAD_GRAYSCALE);
+    
+    if (src.empty())
+    {
+        cerr << "Image load failed!" <<endl;
+        return;
+    }
+    
+    vector<vector<Point>> contours;
+    findContours(src, contours, RETR_LIST, CHAIN_APPROX_NONE);
+    
+    Mat dst;
+    cvtColor(src, dst, COLOR_GRAY2BGR);
+    
+    for (int i = 0; i < contours.size(); i++)
+    {
+        Scalar c(rand() & 255, rand() & 255, rand() & 255);
+        drawContours(dst, contours, i, c, 2);
+    }
+    
+    imshow("src", src);
+    imshow("dst", dst);
+    
+    waitKey();
+    destroyAllWindows();
+}
+
+void contours_hier()
+{
+    Mat src = imread("/Users/hangyojeong/Desktop/contours.bmp", IMREAD_GRAYSCALE);
+    
+    if (src.empty())
+    {
+        cerr << "Image load failed!" <<endl;
+        return;
+    }
+    
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;                // Vec4i is the 4-dimension vector
+    findContours(src, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+    
+    Mat dst;
+    cvtColor(src, dst, COLOR_GRAY2BGR);
+    
+    for (int idx = 0; idx >= 0; idx = hierarchy[idx][0])
+    {
+        Scalar c(rand() & 255, rand() & 255, rand() & 255);
+        drawContours(dst, contours, idx, c, -1, LINE_8, hierarchy);
+    }
+    
+    imshow("src", src);
+    imshow("dst", dst);
+    
+    waitKey();
+    destroyAllWindows();
+}
+
+void setLabel(Mat& img, const vector<Point>& pts, const String& label)
+{
+    Rect rc = boundingRect(pts);
+    rectangle(img, rc, Scalar(0, 0, 255), 1);
+    putText(img, label, rc.tl(), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 255));
+}
+
+void PolygonDetecting()
+{
+    Mat img = imread("/Users/hangyojeong/Desktop/polygon.bmp", IMREAD_COLOR);       // Read Image File only Color, Ignore the transparent part
+    
+    if (img.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return;
+    }
+    
+    Mat gray;
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+    
+    Mat bin;
+    threshold(gray, bin, 200, 255, THRESH_BINARY_INV | THRESH_OTSU);
+    
+    vector<vector<Point>> contours;
+    findContours(bin, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    
+    for (vector<Point>& pts : contours)
+    {
+        if (contourArea(pts) < 400)
+            continue;
+        
+        vector<Point> approx;
+        approxPolyDP(pts, approx, arcLength(pts, true) * 0.02, true);
+        
+        int vtc = (int)approx.size();
+        
+        if (vtc == 3)
+        {
+            setLabel(img, pts, "TRI");
+        }
+        else if (vtc == 4)
+        {
+            setLabel(img, pts, "RECT");
+        }
+        else if (vtc > 4)
+        {
+            double len = arcLength(pts, true);
+            double area = contourArea(pts);
+            double ratio = 4 * CV_PI * area / (len * len);
+            
+            if (ratio > 0.8)
+                setLabel(img, pts, "CIR");
+        }
+    }
+    
+    imshow("img", img);
     
     waitKey();
     destroyAllWindows();
